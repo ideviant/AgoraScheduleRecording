20b1e5c787152305e63d3b3353ebbaba24df6445
ideviant <007wangkai@gmail.com>
2018-07-01 18:34:58 +0800

    add config file

--- scheduleTasks.py
+++ scheduleTasks.py
@@ -14,11 +14,23 @@ uid = 0
 randomint = 2147483699
 expiredts = 0
 
-schedule_table = {
-"2018-07-01 12:55:00": 'i2672130092',
-"2018-07-01 13:25:00": 'i2680272182',
-"2018-07-01 11:25:00": 'i2721746229',
-}
+schedule_table = {}
+
+def loadConfig():
+    time = []
+    channel = []
+
+    for line in open('config.txt').readlines():
+        if line.startswith('2018'):
+            time.append(line.strip('\n'))
+        elif line.startswith('i'):
+            channel.append(line.strip('\n'))
+
+    if len(time) == len(channel) and len(time):
+        for i in range(len(time)):
+            schedule_table[channel[i]] = time[i]
+    else:
+        print "config file error!"
 
 def startRecording(channelName):
     p = Process(target=run_proc, args=(channelName,))
@@ -26,15 +38,20 @@ def startRecording(channelName):
 
 def run_proc(channelName):
     ChannelKey = generateMediaChannelKey(appID, appCertificate, channelName, unixts, randomint, uid, expiredts)
-    cmd = "./recorder_local --appId YOURAPPID --channel {0} --appliteDir ../../bin --channelKey {1} --idle 6000 &".format(channelName, ChannelKey)
+    cmd = "./recorder_local --appId YOUR APPID --channel {0} --appliteDir ../../bin --channelKey {1} --idle 6000 &".format(channelName, ChannelKey)
+    print time.strftime("%Y-%m-%d %H:%M:%S")
     print cmd
-    os.system(cmd)
+#    os.system(cmd)
 
 if __name__ == '__main__':
+    loadConfig()
     scheduler = BackgroundScheduler()
 
     for key, value in schedule_table.items():
-        scheduler.add_job(startRecording, 'date', run_date=key, args=[value])
+        # set run time 5 mins before schedule time
+        run_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.mktime(time.strptime(value, "%Y-%m-%d %H:%M:%S")) - 300))
+        print run_time + ': ' + key
+        scheduler.add_job(startRecording, 'date', run_date=run_time, args=[key])
     scheduler.start()
 
     try:
